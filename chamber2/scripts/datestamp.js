6f7c8662c715e4977ae04cafb8b5ee0581c52068
davidredick <117154619+davidredick@users.noreply.github.com>
2023-01-28 14:23:45 -0800

    a

---
+++ chamber2/scripts/datestamp.js
@@ -0,0 +1,8 @@
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+document.querySelector("#year-field").innerText = currentYear;
+document.querySelector("#last-update-field").innerText = document.lastModified;
+document.querySelector("#current-date").textContent = Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(currentDate);
