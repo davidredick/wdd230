07df890ae5506b979cacff58c361684e3c2bb745
davidredick <117154619+davidredick@users.noreply.github.com>
2023-01-28 19:15:45 -0800

    a

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,5 +1,13 @@
-function dateUK() {
-    let dateNow = new Date();
-    let yearOutput = dateNow.getFullYear();
-    document.getElementById("currentDate").innerHTML = Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(dateNow);
-}
+
+//function dateUK() {
+//    let dateNow = new date();
+//        document.getElementById("currentDate").innerHTML = Intl.DateTimeFormat("en-UK", {dateStyle: "full"}) .format(dateNow);
+
+    }
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    
+    document.querySelector("#year-field").innerText = currentYear;
+    document.querySelector("#last-update-field").innerText = document.lastModified;
+    document.querySelector("#current-date").textContent = Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(currentDate);
--- chamber/scripts/lastmodified.js
+++ chamber/scripts/lastmodified.js
@@ -1,5 +1,4 @@
 function lastUpdated() {
     let lastMod = document.lastModified;
     document.getElementById("lastmodified").innerHTML = lastMod;
-    document.querySelector("current-date").textContent = Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(currentDate);
 }
