d0aadde395a349bbdbfd57aa62b9b8fb6086ea32
davidredick <117154619+davidredick@users.noreply.github.com>
2023-02-02 00:40:23 -0800

    a

--- chamber2/scripts/datestamp.js
+++
@@ -1,6 +0,0 @@
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-
-document.querySelector("#year-field").innerText = currentYear;
-document.querySelector("#last-update-field").innerText = document.lastModified;
-document.querySelector("#current-date").textContent = Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(currentDate);
--- chamber2/scripts/pinnedmenu.js
+++
@@ -1,8 +0,0 @@
-/* const body = document.querySelector("header");
-const observer = new IntersectionObserver( 
-  ([event]) => { event.target.childNodes[1].classList.toggle("menu-pinned", event.intersectionRatio < 1); console.log(event.intersectionRatio)},
-  { threshold: [1] }
-);
-//todo: I think the intersection is not being met.  header-gradient is never allowed to intersect?
-
-observer.observe(body); */
