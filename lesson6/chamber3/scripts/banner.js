554cc5fb19a7188e9d31fbc325d7f0249c692747
davidredick <117154619+davidredick@users.noreply.github.com>
2023-02-18 12:41:39 -0800

    a

---
+++ lesson6/chamber3/scripts/banner.js
@@ -0,0 +1,24 @@
+function headingbanner () {
+
+    let day = new Date().getDay();
+    let header = document.querySelector("header");
+    let dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    if(dayOfTheWeek[day] == "Monday" | dayOfTheWeek[day] == "Tuesday" | dayOfTheWeek[day] == "Saturday") {
+
+        let alertBanner = document.createElement("div");
+        alertBanner.innerHTML = '<span>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</span>';
+        
+        let deleteButton = document.createElement("button");
+        
+        deleteButton.innerHTML = 'X'
+        alertBanner.appendChild(deleteButton);
+        alertBanner.classList.add("alert");
+        header.prepend(alertBanner);
+        deleteButton.addEventListener("click", () => {
+            alertBanner.remove();    
+        });
+
+
+    }
+}
---
+++ lesson6/chamber3/scripts/wind.js
@@ -0,0 +1,20 @@
+function windchill() {
+let windSpeed = 3.1;
+let temperature = 49;
+
+function CalculateWindChill(speed, temp) {
+    if (windSpeed > 3.0 & temperature <= 50) {
+        return 35.74 + .621 * temp - (35.75 * speed ** .16) + .4275 * temp * speed ** .16;
+    }
+    else {
+        return "N/A"
+    }
+}
+
+document.querySelector("#temperature").textContent = temperature;
+document.querySelector("#wind-speed").textContent = windSpeed;
+document.querySelector("#wind-chill").textContent = CalculateWindChill(windSpeed, temperature).toFixed(1);
+document.querySelector("#weather-icon").setAttribute("src", "images/weather-rainy.svg");
+
+
+}
