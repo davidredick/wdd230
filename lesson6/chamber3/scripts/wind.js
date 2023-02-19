e8def5fb4faf9bc3559ab24278ef8c81a7e675c0
davidredick <117154619+davidredick@users.noreply.github.com>
2023-02-18 17:16:56 -0800

    a

--- lesson6/chamber3/scripts/wind.js
+++ lesson6/chamber3/scripts/wind.js
@@ -1,20 +1,21 @@
 function windchill() {
-let windSpeed = 3.1;
-let temperature = 49;
+let windSpeed = 5;
+let temperature = 45;
+let fer = "℉";
 
 function CalculateWindChill(speed, temp) {
     if (windSpeed > 3.0 & temperature <= 50) {
-        return 35.74 + .621 * temp - (35.75 * speed ** .16) + .4275 * temp * speed ** .16;
+        return (35.74 + .621 * temp - (35.75 * speed ** .16) + .4275 * temp * speed ** .16);
     }
     else {
         return "N/A"
     }
 }
 
-document.querySelector("#temperature").textContent = temperature;
+document.querySelector("#temperature").textContent = temperature + fer;
 document.querySelector("#wind-speed").textContent = windSpeed;
 document.querySelector("#wind-chill").textContent = CalculateWindChill(windSpeed, temperature).toFixed(1);
-document.querySelector("#weather-icon").setAttribute("src", "images/weather-rainy.svg");
+/*document.querySelector("#weather-icon").setAttribute("src", "images/weather-rainy.svg");*/
 
 
 }
