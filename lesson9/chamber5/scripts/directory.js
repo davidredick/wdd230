f09f3818898805e348c3294b1e74f8e428d89c3b
davidredick <117154619+davidredick@users.noreply.github.com>
2023-03-04 19:13:08 -0800

    a

--- lesson9/chamber5/scripts/directory.js
+++ lesson9/chamber5/scripts/directory.js
@@ -1,37 +1,40 @@
-const urlll = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const urlll = 'https://brotherblazzard.github.io/canvas-content/latter-day-business.json';
 const urll = 'https://davidredick.com/wdd230/aberdeen.json';
 
-const url = 'https://davidredick.github.io/wdd230/lesson9/chamber5/jsons/aberdeen.json';
+const url = 'https://davidredick.github.io/wdd230/lesson9/chamber5/jsons/aberdeen2.json';
 
 
-async function getProphetData() {
+async function getCompanyData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
-    displayProphets(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
+    console.table(data.business);
+    displaybusiness(data.business);  // note that we reference the business array of the data object given the structure of the json file
   }
 
-  const displayProphets = (prophets) => {
+  const displaybusiness = (business) => {
     const cards = document.querySelector('div.cards'); // select the output container element
   
-    prophets.forEach((prophet) => {
+    business.forEach((business) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let p = document.createElement('p');
       let portrait = document.createElement('img');
   
-      // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${prophet.name} ${prophet.lastname}`;
-      p.innerHTML = `Date of Birth: ${prophet.birthdate}<br>`;
-      p.innerHTML += `Place of Birth: ${prophet.birthplace}<br>`;
-      p.innerHTML += `Date of Death: ${prophet.death}<br>`;
-      p.innerHTML += `Years Served as President: ${prophet.length}<br>`;
-      p.innerHTML += `Number of Children: ${prophet.numofchildren}<br>`;
+      // Build the h2 content out to show the business's full name - finish the template string
+      h2.textContent = `${business.businessname}`;
+
+      p.innerHTML = `Bi-Line: ${business.bi-line}<br>`;
+      p.innerHTML += `Contact Name: ${business.contactname}<br>`;
+      p.innerHTML += `Address 1: ${business.address1}<br>`;
+      p.innerHTML += `Address 2: ${business.address2}<br>`;
+      p.innerHTML += `City State, Zip: ${business.city}, ${business.state} ${business.zipcode}<br>`;
+      p.innerHTML += `Website: ${business.website}<br>`;
+      p.innerHTML += `Membership Level: ${business.membershiplevel}<br>`;
   
       // Build the image portrait by setting all the relevant attribute
-      portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+      portrait.setAttribute('src', business.logoeurl);
+      portrait.setAttribute('alt', `Business Logo of: ${business.businessname}`);
       portrait.setAttribute('loading', 'lazy');
       portrait.setAttribute('width', '340');
       portrait.setAttribute('height', '440');
@@ -45,4 +48,4 @@ async function getProphetData() {
     }) // end of forEach loop
 } // end of function expression
   
-  getProphetData();
+  getCompanyData();
